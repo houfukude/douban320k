886c6859e62dcb622e59d2003396590d9598e2e5
houfukude <kuagedmacosx@gmail.com>
2014-08-05 17:26:11 +0800

    Update douban320k.py

--- douban320k.py
+++ douban320k.py
@@ -124,27 +124,27 @@ def find_first_url(result,artist,album):
 def get_palyback_url(channel,sid):
 	url = add_userinfo(playlist_url)
 	if url == playlist_url:
-		return u'[INFO]:你还没登陆！'
+		return u'\t[INFO]:你还没登陆！'
 	return url +'&channel='+ channel +'&sid='+sid
 
-def run_callback(callback_url,type,like = '0'):
+def run_callback(callback_url,type,like = '-1'):
 	if int(like) == 1 and type == 'r':
-		return u'[INFO]:已红心过了！'
+		return u'\t[INFO]:已红心过了！'
 	if int(like) == 0 and type == 'u':
-		return u'[INFO]:未红心过了！'
+		return u'\t[INFO]:未曾红心过！'
 	if '[INFO]' not in callback_url:
 		callback_url = callback_url+'&type='+type
 		th = threading.Thread(target = get_data,args = (callback_url,))
 		th.start()
-		return {0:u'[INFO]:已红心标记！',1:u'[INFO]:已取消红心！',}[like]
+		return {-1:u'\t[INFO]:已完成服务器反馈！',0:u'\t[INFO]:已红心标记！',1:u'\t[INFO]:已取消红心！',}[int(like)]
 	else:
 		return callback_url
 	
-def kbfunc(): 
-	ret = 'p'
+def keyboard_func(): 
+	ret = -1
 	x = msvcrt.kbhit()
 	if x: 
-		ret = msvcrt.getch()
+		ret = ord(msvcrt.getch())
 	else:
 		time.sleep(1)
 	return ret
@@ -154,58 +154,71 @@ def play(msg,url,callback_url,like):
 	try:
 		print msg.encode('gb18030')
 	except:
-		print 'msg print error'
-		fp = open('error'+time.strftime('%Y%m%d',time.localtime(time.time()))+'.log','wb')
+		errorfile = 'error'+time.strftime('%Y%m%d',time.localtime(time.time()))+'.log'
+		print 'msg print error at',errorfile
+		fp = open(errorfile,'wb')
 		fp.write(msg)
 		fp.close()
-	print '\t',{1:u'[INFO]:已红心！',0:u'[INFO]:未红心！',}[like]
-	like_status = like
+	print {1:u'\t[INFO]:已红心！',0:u'\t[INFO]:未红心！',}[int(like)]
 	#os.system("title [*]nowplaying...")
-	playing = subprocess.Popen('mpg123 -qv '+url, shell = False)
+	playing = subprocess.Popen('mpg123 -qv '+url,stdin = subprocess.PIPE, shell = False)
+	type = 'e'
+	like_status = like
 	while playing.poll() is None:
-		cmd = kbfunc()
-		type = None
-		if(cmd is ' '):
+		key = keyboard_func()
+		if(key == 32):
 			#TODO pause playing
+			#playing.stdin.write(' ')
 			pass
-		if(cmd is 'r')or (cmd is 'R'):
-			os.system('cls')
-			try:
+		if (key == 224):#NoneABC
+			key = ord(msvcrt.getch())
+			if (key == 77): #Right arrow
+				playing.kill()
+				type = 's'
+				break
+			if (key == 72): #Up arrow
+				os.system('cls')
 				print msg.encode('gb18030')
-			except:
-				pass
-			print '\t',run_callback(callback_url,'r',like_status)
+				print run_callback(callback_url,'r',like_status)
+				like_status = 1
+			if (key == 80): #Down arrow
+				os.system('cls')
+				print msg.encode('gb18030')
+				print run_callback(callback_url,'u',like_status)
+				like_status = 0
+			if (key == 75): #Left arrow
+				playing.kill()
+				os.system('cls')
+				start()
+		if(key == 82) or (key == 114):#R r
+			os.system('cls')
+			print msg.encode('gb18030')
+			print run_callback(callback_url,'r',like_status)
 			like_status = 1
-		if(cmd is 'b') or (cmd is 'B'):
+		if(key == 66) or (key == 98):#B b
 			playing.kill()
-			run_callback(callback_url,'b')
-			#os.system("title [*]find a new song...")
+			type = 'b'
 			break
-		if(cmd is 'n')or (cmd is 'N'):
+		if(key == 78) or (key == 110):#N n
 			playing.kill()
-			run_callback(callback_url,'s')
-			#os.system("title [*]find a new song...")
+			type = 's'
 			break
-		if(cmd is 'c')or (cmd is 'C'):
-			run_callback(callback_url,'b')
+		if(key == 67) or (key == 99):#C c
 			playing.kill()
 			os.system('cls')
 			start()
-		if(cmd is 'u')or (cmd is 'U'):
+		if(key == 85) or (key == 117):#U u
 			os.system('cls')
-			try:
-				print msg.encode('gb18030')
-			except:
-				pass
-			print '\t',run_callback(callback_url,'u',like_status)
+			print msg.encode('gb18030')
+			print run_callback(callback_url,'u',like_status)
 			like_status = 0
-		if(cmd is 'q')or (cmd is 'Q'):
+		if(key == 81) or (key == 113) or (key == 27):#Q q Esc
 			playing.kill()
-			run_callback(callback_url,'b')
 			print '\n\nBye Bye!'
+			type = 'b'
 			sys.exit()
-	run_callback(callback_url,'e')
 	os.system('cls')
+	print run_callback(callback_url,type)
 	print u'\t正在载入下一曲...'
 
 # 播放douban.fm
@@ -225,7 +238,7 @@ def play_channel(channel='0',type='n',mode ='0'):
 		if(album == u'豆瓣FM'):
 			return
 		if mode == '2':
-			msg  = u' [@] 64K '
+			msg  = u'\t[@] 64K '
 			url = douban_url
 		else:
 			result = search_song_by_name(title)
@@ -234,13 +247,13 @@ def play_channel(channel='0',type='n',mode ='0'):
 			url = find_matched_url(result,artist,album)	
 			if url == None:
 				if mode == '0':
-					msg  = u' [@] 64K '
+					msg  = u'\t[@] 64K '
 					url = douban_url
 				if mode == '1':
-					msg = u' [@] 320K '
+					msg = u'\t[@] 320K '
 					url = find_first_url(result,artist,album)
 			else:
-				msg = u' [*] 320K '
+				msg = u'\t[*] 320K '
 		msg = msg + u'[正在播放]:%s \n\t[艺术家]:%s [专辑]:%s' % (title.encode('utf-8'), artist.encode('utf-8'), album.encode('utf-8'))
 		callback_url = get_palyback_url(channel,sid)
 		play(msg,url,callback_url,like)
@@ -252,14 +265,14 @@ def start():
 	global userinfo
 	channel = '0'
 	mode = '0'
-	msg = u'\n[ q ] \t系统退出\t\t'
+	msg = u'[ q ] \t系统退出\t\t[-3]\t%s的红心收藏'
 	os.system("title Douban320k player")
 	if len(sys.argv) < 3:
 		if os.path.isfile(cfg):
 			data = file(cfg).read().split(' ### ')
 			userinfo = data[0]
 			res = json.loads( userinfo)
-			msg = msg+ u'[-3]\t%s的红心收藏'% ( res['user_name'])
+			msg = msg % ( res['user_name'])
 			mode = data[1]
 	else:
 		email = sys.argv[1]
@@ -271,7 +284,7 @@ def start():
 		userinfo = login_douban(email,password)
 		res = json.loads( userinfo)
 		if res['r'] != 1:
-			msg = msg + u'[-3]\tchannel:%s的红心收藏'% ( res['user_name'])
+			msg = msg % ( res['user_name'])
 			data = userinfo +" ### "+mode
 			fp = open(cfg,'wb')
 			fp.write(data.encode('utf-8'))
@@ -289,9 +302,11 @@ def start():
 			print '[%2d]\tchannel:%s' % (id,name),
 			if(len(name)<=3):
 				print '\t',
+			if (i == range(len(channels['channels']))):
+				print ''
 		else:
 			print '\t[%2d]\tchannel:%s' % (id,name)
-		if(id ==16) or (id == 78):
+		if(id ==16) or (id == 77):
 			print '\t',
 	#
 	print msg
